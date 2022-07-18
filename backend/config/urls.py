c6dc1ac78a37bbd7d6da339269875cd155ff418d
smoll <smolll123@mail.ru>
2022-07-18 13:03:19 +0300

    fix: make User's profile photo and token login

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
+from djoser.urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -9,5 +9,14 @@ class UserRegistrationSerializer(BaseUserRegistrationSerializer):
         fields = tuple(User.REQUIRED_FIELDS) + (
             settings.LOGIN_FIELD,
             settings.USER_ID_FIELD,
-            "password", "username"
+            'password', 'username', 'photo'
         )
+
+
+"""
+Так не работает
+class UserRegistrationSerializer(BaseUserRegistrationSerializer):
+    class Meta(BaseUserRegistrationSerializer.Meta):
+        model = User
+        fields = '__all__'
+"""
